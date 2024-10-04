027a6524156bf47c3ff228abb02e25c6c73868ed
Soup47 <gideonng81@gmail.com>
2024-10-04 19:04:10 +0800

    centralised motor cartridges

--- include/mechLib.hpp
+++ include/mechLib.hpp
@@ -1,11 +1,4 @@
 #ifndef _MECHLIB_HPP_
 #define _MECHLIB_HPP_
 
-// #define TowerKP 1
-
-// void TowerPID (void*ignore);
-void cataPID (void*ignore);
-void cataControl();
-void changeTower (double p_targArm);
-
 #endif
