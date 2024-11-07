6c1c27e92f3aa7fca200dc125f91b18812614dd1
Soup47 <gideonng81@gmail.com>
2024-11-07 15:56:59 +0800

    Moved motor reverse to globals

--- include/globals.hpp
+++ include/globals.hpp
@@ -4,16 +4,22 @@
 
 #define FLPORT 19                 // [front right wheel]
 #define FLGEAR E_MOTOR_GEARSET_18 // [front left gearset]
+#define FLREVERSE false           // [front left reverse]
 #define FRPORT 13                 // [front left wheel]
 #define FRGEAR E_MOTOR_GEARSET_18 // [front right gearset]
-#define BLPORT 9                  // [back  left wheel]
+#define FRREVERSE false           // [front right reverse]
+#define BLPORT 9                  // [back left wheel]
 #define BLGEAR E_MOTOR_GEARSET_18 // [back left gearset]
-#define BRPORT 1                  // [back  right wheel]
+#define BLREVERSE false           // [back left reverse]
+#define BRPORT 1                  // [back right wheel]
 #define BRGEAR E_MOTOR_GEARSET_18 // [back right gearset]
+#define BRREVERSE false           // [Back right reverse]
 #define MLPORT 10                 // [middle left wheel]
 #define MLGEAR E_MOTOR_GEARSET_18 // [middle left gearset]
+#define MLREVERSE false           // [middle left reverse]
 #define MRPORT 2                  // [middle right wheel]
 #define MRGEAR E_MOTOR_GEARSET_18 // [middle right gearset]
+#define MRREVERSE false           // [middle right reverse]
 
 
 #endif
